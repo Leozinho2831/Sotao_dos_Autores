b9f84e1a4659006482c3793655a617ed05b12087
Leozinho2831 <leoferraz1818@gmail.com>
2023-08-08 22:54:23 -0300

    Commit: slide links

--- Assets/Javascript/Slide.js
+++ Assets/Javascript/Slide.js
@@ -1,20 +1,52 @@
-const imgs = document.getElementById("slide");
-const img = document.querySelectorAll("#slide img");
-        
+const initialLinks = [
+    'Assets/Pages/Capítulos-second-life.html',
+    'Assets/Pages/Capítulos-Magnum.html',
+    'Assets/Pages/Capítulos-Imperfeito.html'
+];
+
+const imgContainer = document.getElementById("slide");
+const imgList = imgContainer.querySelectorAll("img");
 let idx = 0;
+let isTransitionInProgress = false;
 
-function slide(){
-    idx++;
+function slide() {
+    if (isTransitionInProgress) {
+        return;
+    }
 
-    if (idx > img.length - 1){
-         idx = 0;
+    let nextIdx = idx + 1;
+
+    if (nextIdx > imgList.length - 1) {
+        nextIdx = 0;
     }
 
     const windowWidth = window.innerWidth;
 
     const responsiveOffset = 550 * (windowWidth <= 570 ? 0.453 : 0.98);
 
-    imgs.style.transform = `translateX(${-idx * responsiveOffset}px)`;
+    imgContainer.style.transform = `translateX(${-nextIdx * responsiveOffset}px)`;
+    isTransitionInProgress = true;
+
+    const link = document.getElementById("linkAnchor");
+    link.href = initialLinks[nextIdx];
 }
 
-setInterval(slide, 4800);
+const initialLink = document.getElementById("linkAnchor");
+initialLink.href = initialLinks[0];
+
+setInterval(slide, 3600);
+
+imgContainer.addEventListener('transitionend', () => {
+    idx++;
+
+    if (idx > imgList.length - 1) {
+        idx = 0;
+    }
+
+    isTransitionInProgress = false;
+
+    const link = document.getElementById("linkAnchor");
+    link.href = initialLinks[idx];
+});
+
+
