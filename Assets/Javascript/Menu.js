af7aba90f96dc6584a6cdc732bb274198ce50252
Leozinho2831 <129678079+Leozinho2831@users.noreply.github.com>
2023-08-11 20:55:28 -0300

    commit: contacts

--- Assets/Javascript/Menu.js
+++
@@ -1,8 +0,0 @@
-function mobileMenu() {
-    let menuMobile = document.querySelector('.mobile__menu');
-    if (menuMobile.classList.contains('view__menu')){
-        menuMobile.classList.remove('view__menu');
-    } else {
-        menuMobile.classList.add('view__menu');
-    }
-}
