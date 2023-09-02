d87b732918dc0cd1d7578c7ad66c531c3716e1f3
Leozinho2831 <leoferraz1818@gmail.com>
2023-09-02 16:30:09 -0300

    commit: new slide

--- Assets/Javascript/Slide.js
+++ Assets/Javascript/Slide.js
@@ -1,57 +1,31 @@
-const initialLinks = [
-    'Assets/Pages/Capítulos-second-life.html',
-    'Assets/Pages/Capítulos-Magnum.html',
-    'Assets/Pages/Capítulos-Imperfeito.html',
-    'Assets/Pages/Capítulos-Destino.html'
-];
+// const initialLinks = [
+//     'Assets/Pages/Capítulos-second-life.html',
+//     'Assets/Pages/Capítulos-Magnum.html',
+//     'Assets/Pages/Capítulos-Imperfeito.html',
+//     'Assets/Pages/Capítulos-Destino.html'
+// ];
 
-const imgContainer = document.getElementById("slide");
-const imgList = imgContainer.querySelectorAll("img");
-let idx = 0;
-let isTransitionInProgress = false;
+// slide 3600ms
 
-function slide() {
-    if (isTransitionInProgress) {
-        return;
-    }
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
-    let nextIdx = idx + 1;
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 
-    if (nextIdx > imgList.length - 1) {
-        nextIdx = 0;
-    }
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
 
-    const windowWidth = window.innerWidth;
-    let responsiveOffset;
-
-    if (windowWidth < 560) {
-        responsiveOffset = 550 * 0.4544;
-    } else {
-        responsiveOffset = 550 * 0.9901;
-    }
-
-    imgContainer.style.transform = `translateX(${-nextIdx * responsiveOffset}px)`;
-    isTransitionInProgress = true;
-
-    const link = document.getElementById("linkAnchor");
-    link.href = initialLinks[nextIdx];
-}
-
-const initialLink = document.getElementById("linkAnchor");
-initialLink.href = initialLinks[0];
-
-setInterval(slide, 3600);
-
-imgContainer.addEventListener('transitionend', () => {
-    idx++;
-
-    if (idx > imgList.length - 1) {
-        idx = 0;
-    }
-
-    isTransitionInProgress = false;
-
-    const link = document.getElementById("linkAnchor");
-    link.href = initialLinks[idx];
+    keyboard: {
+        enabled: true,
+    },
 });
-
